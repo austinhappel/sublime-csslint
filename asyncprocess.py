8f197a327cb6bfad781ee0d2ef56ce214adf8558
Austin Happel <austin@austinhappel.com>
2013-08-07 22:49:33 -0700

    * PEP8 formatting (indentation mostly) * conditional import of thread to support python 2 and 3 * added test css file (no actual tests written)

--- asyncprocess.py
+++ asyncprocess.py
@@ -1,7 +1,7 @@
 try:
-  import _thread as thread
+    import _thread as thread
 except ImportError:
-  import thread
+    import thread
 import os
 
 import subprocess
@@ -9,45 +9,46 @@ import functools
 import time
 import sublime
 
+
 class AsyncProcess(object):
-  def __init__(self, cmd, listener):
-    self.cmd = cmd
-    self.listener = listener
-    #print "DEBUG_EXEC: " + str(self.cmd)
+    def __init__(self, cmd, listener):
+        self.cmd = cmd
+        self.listener = listener
+        #print "DEBUG_EXEC: " + str(self.cmd)
 
-    self.proc = subprocess.Popen(self.cmd, env={"PATH": os.environ['PATH']}, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        self.proc = subprocess.Popen(self.cmd, env={"PATH": os.environ['PATH']}, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-    if self.proc.stdout:
-      thread.start_new_thread(self.read_stdout, ())
+        if self.proc.stdout:
+            thread.start_new_thread(self.read_stdout, ())
 
-    if self.proc.stderr:
-      thread.start_new_thread(self.read_stderr, ())
+        if self.proc.stderr:
+            thread.start_new_thread(self.read_stderr, ())
 
-    thread.start_new_thread(self.poll, ())
+        thread.start_new_thread(self.poll, ())
 
-  def poll(self):
-    while True:
-      if self.proc.poll() != None:
-        sublime.set_timeout(functools.partial(self.listener.proc_terminated, self.proc), 0)
-        break
-      time.sleep(0.1)
+    def poll(self):
+        while True:
+            if self.proc.poll() is None:
+                sublime.set_timeout(functools.partial(self.listener.proc_terminated, self.proc), 0)
+                break
+            time.sleep(0.1)
 
-  def read_stdout(self):
-    while True:
-      data = os.read(self.proc.stdout.fileno(), 2**15)
-      if data != "":
-        sublime.set_timeout(functools.partial(self.listener.process_data, self.proc, data), 0)
-      else:
-        self.proc.stdout.close()
-        self.listener.is_running = False
-        break
+    def read_stdout(self):
+        while True:
+            data = os.read(self.proc.stdout.fileno(), 2**15)
+            if data != "":
+                sublime.set_timeout(functools.partial(self.listener.process_data, self.proc, data), 0)
+            else:
+                self.proc.stdout.close()
+                self.listener.is_running = False
+                break
 
-  def read_stderr(self):
-    while True:
-      data = os.read(self.proc.stderr.fileno(), 2**15)
-      if data != "":
-        sublime.set_timeout(functools.partial(self.listener.process_data, self.proc, data), 0)
-      else:
-        self.proc.stderr.close()
-        self.listener.is_running = False
-        break
+    def read_stderr(self):
+        while True:
+            data = os.read(self.proc.stderr.fileno(), 2**15)
+            if data != "":
+                sublime.set_timeout(functools.partial(self.listener.process_data, self.proc, data), 0)
+            else:
+                self.proc.stderr.close()
+                self.listener.is_running = False
+                break
